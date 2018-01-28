83efb9cee55a00de80d42ddc0ca276d5ae6be236
Zachery Miranda <exsmiley@gmail.com>
2018-01-28 00:00:08 -0500

    yarghhhhh new graph sorta works

--- app.py
+++ app.py
@@ -1,10 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+import json
 app = Flask(__name__)
+
+# config
+app.config['DEBUG'] = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+
+# load all of the data
+with open('data/processed_pmid.json') as f:
+    pmid_data = json.loads(f.read())
+
+with open('data/nodes.json') as f:
+    node_data = json.loads(f.read())
+
+with open('data/edges.json') as f:
+    edge_data = json.loads(f.read())        
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/api/nodes')
+def get_nodes():
+    return jsonify(node_data)
+
+
+@app.route('/api/edges')
+def get_edges():
+    return jsonify(edge_data)
+
+
+@app.route('/api/all_pmids')
+def get_all_pmids():
+    return jsonify(pmid_data)
+
+
+@app.route('/api/pmid')
+def get_pmid():
+    pmid = request.args.get('pmid')
+    return jsonify(pmid_data[pmid])
 
 
 if __name__ == '__main__':
